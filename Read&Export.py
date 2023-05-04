4b9e8c30146739d07e917280349ac9243b9dc116
Wreckingbald <ben97163@gmail.com>
2023-05-05 02:51:45 +0800

    加了字串的features

--- Read&Export.py
+++ Read&Export.py
@@ -1,21 +1,34 @@
 import numpy as np
 import joblib
 import pandas as pd
+import category_encoders as ce
+
 #產出CSV檔                
 def OutputCSV(data):   
-    Result ='ML\FinalProject\\output.csv'
+    Result ='ML\FinalProject\\20features.csv'
     data.to_csv(Result, index=False)
     print('Export Succeeded: ' + Result)
 
 '''讀檔'''
-loaded_model = joblib.load('ML\FinalProject\'GBDT2')  # 把模型輸在這裡
+loaded_model = joblib.load('ML\FinalProject\GBDTwith20features')  # 把模型輸在這裡
 data = pd.read_csv("ML\FinalProject\\test.csv", sep = ",") #測試檔案
+data_train = pd.read_csv("ML\FinalProject\\train.csv", sep = ",")
+
+target_encode = ['Album_type','Licensed','official_video', 'Track', 'Album', 'Channel', 'Composer', 'Artist']
+data['Album_type'].fillna('album', inplace = True)
+data['Licensed'].fillna(True, inplace = True)
+data['official_video'].fillna(True, inplace = True)
+for i in target_encode:
+    target_enc = ce.TargetEncoder(cols=i, drop_invariant=True)
+    target_enc.fit(data_train[i], data_train['Danceability'])
+    data[i] = target_enc.transform(data[i])
 
 tags = data.columns
 for i in range(13):
     data[tags[i]].fillna(np.mean(data[tags[i]]), inplace = True)
 data[['Key', 'Duration_ms', 'Views', 'Likes', 'Stream']] = data[['Key', 'Duration_ms', 'Views', 'Likes', 'Stream']].round(0)
-X = data[tags[0:12]]
+X = data[tags[0:12]].join(data[target_encode])
+y = data[tags[0]]
 
 ''''輸出.csv'''
 y_pred = loaded_model.predict(X)
--- preprocess.py
+++ preprocess.py
@@ -2,31 +2,34 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import collections
-from sklearn.preprocessing import LabelEncoder
+import category_encoders as ce
 data = pd.read_csv("ML\FinalProject\\train.csv", sep = ",")
 
-target_encode = ['Album_type','Licensed','official_video']
-# Create new columns in clicks using preprocessing.LabelEncoder()
-encoder = LabelEncoder()
-for feature in target_encode:
-    encoded = encoder.fit_transform(data[feature])
-    data[feature] = encoded
+target_encode = ['Album_type','Licensed','official_video', 'Track', 'Album', 'Channel', 'Composer', 'Artist']
+data['Album_type'].fillna('album', inplace = True)
+data['Licensed'].fillna(True, inplace = True)
+data['official_video'].fillna(True, inplace = True)
+for i in target_encode:
+    target_enc = ce.TargetEncoder(cols=i, drop_invariant=True)
+    target_enc.fit(data[i], data['Danceability'])
+    data[i] = target_enc.transform(data[i])
 
 tags = data.columns
-for i in range(17):
+for i in range(14):
     data[tags[i]].fillna(np.mean(data[tags[i]]), inplace = True)
 data[['Key', 'Duration_ms', 'Views', 'Likes', 'Stream', 'Danceability']] = data[['Key', 'Duration_ms', 'Views', 'Likes', 'Stream', 'Danceability']].round(0)
-
-
-print(collections.Counter(data['Album_type'])) # {'album': 10379, 'single': 3732, nan: 2560, 'compilation': 499}
-'''print(collections.Counter(data['Licensed']))  # {True: 10260, False: 4317, nan: 2593}
-print(collections.Counter(data['official_video'])) # {True: 11491, False: 3064, nan: 2615}
-print(collections.Counter(data['Track']))
-print(collections.Counter(data['Album']))
-print(collections.Counter(data['Channel']))
-print(collections.Counter(data['Composer']))
-print(collections.Counter(data['Artist']))'''
-
-X = data[tags[1:16]]
+X = data[tags[1:13]].join(data[target_encode])
 y = data[tags[0]]
-print(X.loc[0])
+
+print(len(X.loc[0]))
+
+'''
+print(collections.Counter(data['Album_type'])) # {'album': 10379, 'single': 3732, nan: 2560, 'compilation': 499}
+print(collections.Counter(data['Licensed']))  # {True: 10260, False: 4317, nan: 2593}
+print(collections.Counter(data['official_video'])) # {True: 11491, False: 3064, nan: 2615}
+print(len(collections.Counter(data['Track'])))
+print(len(collections.Counter(data['Album'])))
+print(len(collections.Counter(data['Channel'])))
+print(len(collections.Counter(data['Composer'])))
+print(len(collections.Counter(data['Artist'])))
+'''
