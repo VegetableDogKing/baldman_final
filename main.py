f27d1a5de702225e3851551e82730206d2b8d5b0
Wreckingbald <ben97163@gmail.com>
2023-05-18 19:04:20 +0800

    new

--- main.py
+++ main.py
@@ -16,15 +16,20 @@ train_path, test_path = os.path.join(absolute_path, relative_path), os.path.join
 traindata, testdata = pd.read_csv(train_path, sep = ","), pd.read_csv(test_path, sep = ",")
 
 
-y = traindata['Danceability']
-traindata.pop("Danceability")
-X, Xtest = traindata, testdata
+y = traindata.pop('Danceability')
+useless = ['Uri', 'Url_spotify', 'Url_youtube', 'Description']
+X, Xtest = traindata.drop(useless, axis=1), testdata.drop(useless, axis=1)
+
 
 ONEHOTarr = ['Album_type','Licensed','official_video']
 for feature in ONEHOTarr:
-    X[feature], Xtest[feature] = util.OneHotEncoding(X[feature], y, Xtest[feature])
-Catarr = ['Track', 'Album', 'Channel', 'Composer', 'Artist']
-for feature in ONEHOTarr:
+    tmp1, tmp2 = util.OneHotEncoding(X.pop(feature), y, Xtest.pop(feature))
+    X, Xtest = pd.concat([X, tmp1], axis=1), pd.concat([Xtest, tmp2], axis=1)
+    
+Catarr = ['Track', 'Album', 'Channel', 'Composer', 'Artist', 'Title']
+for feature in Catarr:
     X[feature], Xtest[feature] = util.CatBoostEncoding(X[feature], y, Xtest[feature])
 
-X, Xtest = util.fillna(traindata, testdata)
+print(X.iloc[0])
+
+# X, Xtest = util.fillna(traindata, testdata)
