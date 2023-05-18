a255f47852612a6ed0c8ba499be08e0bf5f90eef
Wreckingbald <ben97163@gmail.com>
2023-05-18 23:00:21 +0800

    outputcsv

--- util.py
+++ util.py
@@ -4,8 +4,11 @@ import matplotlib.pyplot as plt
 import collections
 import category_encoders as ce
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 import seaborn as sns
 import os
+file_name = 'Logistic_Regression'
+
 absolute_path = os.path.dirname(__file__)
 relative_path = "train.csv"
 file_path = os.path.join(absolute_path, relative_path) 
@@ -45,6 +48,9 @@ def preprocess(data: pd.DataFrame, encode_arr = [], y = None):
     return X, encode_arr
 
 def fillna(X: pd.DataFrame, y: pd.DataFrame, Xtest:pd.DataFrame):
+    for feature in X.columns:
+        X[feature].fillna(np.mean(X[feature]), inplace = True)
+        Xtest[feature].fillna(np.mean(Xtest[feature]), inplace = True)    
     return X, Xtest
 
 def OneHotEncoding(X: pd.DataFrame, y: pd.DataFrame, Xtest:pd.DataFrame): 
@@ -67,6 +73,25 @@ def Standardize(X: pd.DataFrame, Xtest:pd.DataFrame):
     Xtest = scaler.fit_transform(Xtest)
     return X, Xtest
 
+def Normalize(X: pd.DataFrame, Xtest:pd.DataFrame):
+    pass
+
+def _PCA(X: pd.DataFrame, Xtest:pd.DataFrame, components):
+    pca = PCA(n_components=components)
+    pca.fit(X)
+    X, Xtest = pca.transform(X), pca.transform(Xtest)
+    return X, Xtest
+
+#產出CSV檔                
+def OutputCSV(y_pred: pd.DataFrame):
+    label = range(17170,23485)
+    df = {"id":label, "Danceability": y_pred}
+    df = pd.DataFrame(df)
+    Result ='ML\FinalProject\\outputs\\' + file_name + '.csv'
+    df.to_csv(Result, index=False)
+    print('Export Succeeded: ' + Result)
+    return
+
 def correlation(df_feat: pd.DataFrame):
     corr_df = df_feat.corr()
     fig = plt.figure(figsize=(20, 8))
@@ -84,14 +109,3 @@ def correlation(df_feat: pd.DataFrame):
     df_feat = np.array(df_feat)
     
     return df_feat, tags
-
-'''
-print(collections.Counter(data['Album_type'])) # {'album': 10379, 'single': 3732, nan: 2560, 'compilation': 499}
-print(collections.Counter(data['Licensed']))  # {True: 10260, False: 4317, nan: 2593}
-print(collections.Counter(data['official_video'])) # {True: 11491, False: 3064, nan: 2615}
-print(len(collections.Counter(data['Track'])))
-print(len(collections.Counter(data['Album'])))
-print(len(collections.Counter(data['Channel'])))
-print(len(collections.Counter(data['Composer'])))
-print(len(collections.Counter(data['Artist'])))
-'''
